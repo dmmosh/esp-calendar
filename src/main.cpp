91f097b6be65e2e8f6bcc1782925e7f08ab8a3bb
dmmosh <evilsharkeater@gmail.com>
2025-07-02 20:31:31 -0400

    fdjsndkj

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LED 2
+#define pause(x) vTaskDelay(x/portTICK_PERIOD_MS)
+
+
+void setup() {
+  // put your setup code here, to run once:
+  while(true){
+    digitalWrite(LED,HIGH);
+    delay(1000);
+    digitalWrite(LED,LOW);
+    delay(1000);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
